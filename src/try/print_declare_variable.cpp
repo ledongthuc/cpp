b558fa2de4047c87e141bedb245ecc0eec6390a0
Thuc Le <ledongthuc@gmail.com>
2017-01-19 15:33:44 +0700

    Update code

---
+++ src/try/print_declare_variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a;
+  cout << "int a: " << a << std::endl;
+
+  float b;
+  cout << "int b: " << b << std::endl;
+
+  string c;
+  cout << "int c: " << c << std::endl;
+
+  return 0;
+}
